c2e426d0747a1f07775b1b5e0da19d5d0d1118d6
DigitalStyx918 <51181299+DigitalStyx918@users.noreply.github.com>
2019-07-25 20:04:59 -0500

    Update Three.js

--- examples/basic/js/Three.js
+++ examples/basic/js/Three.js
@@ -1,4 +1,5 @@
 // Three.js r46 - http://github.com/mrdoob/three.js
+Access-Control-Allow-Origin: *
 var THREE = THREE || {};
 if (!self.Int32Array)self.Int32Array = Array, self.Float32Array = Array;
 THREE.Clock = function (a) {
